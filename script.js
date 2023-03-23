5b94ad256c8e38618e0c328d8e43adbf2d205776
Anurag Sharma <anuragsaitiya45@gmail.com>
2023-03-24 04:01:15 +0530

    start learning js

---
+++ script.js
@@ -0,0 +1,19 @@
+const uid = "abc123";
+
+var firstName = "Anurag";
+var secondName = "Sharma";
+var email = "anuragsaitiya32@gmai.com";
+var phone = "9812480563"
+var course = "MCA"
+var password = "123456"
+
+console.log(`
+    // uniq id is = ${uid}
+    // first Name = ${firstName}
+    // last Name = ${secondName}
+    email is = ${email}
+    mobile number = ${phone}
+    course = ${course}
+    password is = ${password}
+
+`);
