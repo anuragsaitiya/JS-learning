35b8881c9b301853950c79231ab98c06517543f6
Anurag Sharma <anuragsaitiya45@gmail.com>
2023-03-24 08:45:23 +0530

    operators learning

---
+++ Logical conditional.js
@@ -0,0 +1,10 @@
+let email = false;
+let google = false;
+let facebook = false;
+
+if (email || google || facebook) {
+    console.log("Welcome");
+} else {
+    console.log("access denied");
+
+}
---
+++ Ternary operator.js
@@ -0,0 +1,11 @@
+var authenticated = true;
+
+
+// if (authenticated) {
+//     console.log("SignOut btn");
+// } else {
+//     console.log("Login btn");
+// }
+
+
+authenticated ? console.log("SignOut btn") : console.log("Login btn");
